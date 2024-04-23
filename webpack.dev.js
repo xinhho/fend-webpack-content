20265ef4e565fb8e3429f6b4fe07a88f7ce4100d
XinhHo <xinhho1995@gmail.com>
2024-04-23 14:12:19 +0700

    commit code

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,35 +1,25 @@
-const path = require('path')
-const webpack = require('webpack')
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path")
+const webpack = require("webpack")
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: './src/client/index.js',
-    mode: 'development',
-    devtool: 'source-map',
-    stats: 'verbose',
-    module: {
-        rules: [
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/client/index.js',
+  module: {
+    rules: [
             {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
             }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        })
     ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html"
+    })
+  ]
 }
